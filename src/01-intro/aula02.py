0995d3a23c513580645469aeaa9b2a435328a0b2
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-06 14:12:01 -0300

    aula 01 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,24 @@
+""" Aula 02 - Keywords e identificadores """
+
+#Palavras reservadas
+#None, false, true, and, import, lambda
+
+#Identificadores
+#nome de variáveis, funções, classes
+#case sensitive
+#devem iniciar com letra ou _
+#não pode ter espços em branco
+#separa com _
+#não pode usar carcateres especiais
+
+#Boas práticas (clean code)
+#nome explícito para as variáveis
+#constantes em letras maiúsculas (PI = 3.14)
+
+MAIORIDADE = 18
+idade = 15
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
