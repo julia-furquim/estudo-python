f155e5620bde48aa6d9e9b63603dbad40a716ec8
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-16 20:24:49 -0300

    debug dos exercícios

--- src/04-funcoes/exercicios/ex01.py
+++ src/04-funcoes/exercicios/ex01.py
@@ -1,7 +1,7 @@
 """ crie uma função que recebe três números como 
 parâmetro (n1, n2, n3) e imprime na saída padrão a soma dos 
 números """
-
+breakpoint()
 def somar(n1, n2, n3):
     print(f'O resultado da soma dos números {n1}, {n2}, {n3} é igual a:\n', n1 + n2 + n3)
 
