7dbf445929cf682c415cd0350322ff0f207c805f
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-16 10:45:03 -0300

    exercício 4

---
+++ src/04-funcoes/exercicios/ex04.py
@@ -0,0 +1,10 @@
+"""  crie uma função que recebe vários argumentos numéricos 
+através do *args retorna a soma dos números """
+
+def somar(*numeros):
+    soma = 0.0
+    for numero in numeros:
+        soma += numero
+    return soma
+
+print('O resultado da soma dos números é igual a', somar(10.0, 5.5, 8.0, 2.3, 10.5, 6.0))
