066933c3db0b8be592e7709138a5d5104d5cf1ac
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-16 20:27:01 -0300

    debug dos exercícios

--- src/04-funcoes/exercicios/ex02.py
+++ src/04-funcoes/exercicios/ex02.py
@@ -1,6 +1,6 @@
 """  crie uma função que recebe três 
 números como parâmetro (n1, n2, n3) e retorna a soma dos números  """
-
+breakpoint()
 def somar(n1, n2, n3):
     resultado = n1 + n2 + n3
     return f'O resultado da soma dos números {n1}, {n2}, {n3} é igual a {resultado}'
