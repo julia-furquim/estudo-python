4373e7b263955461a4234cb07a5df5acb1188722
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-16 20:28:10 -0300

    debug dos exercícios

--- src/04-funcoes/exercicios/ex03.py
+++ src/04-funcoes/exercicios/ex03.py
@@ -1,7 +1,7 @@
 """ crie uma função que recebe uma tupla 
 de números como parâmetro (numeros) e retorna a soma dos números """
 
-
+breakpoint()
 def somar(numeros):
     soma = 0.0
     for numero in numeros:
@@ -10,4 +10,4 @@ def somar(numeros):
 
 numeros_tupla = (10.0, 5.5, 8.0, 2.3, 10.5, 6.0)
 
-print('O resultado da soma dos números da tupla é igual a', somar(numeros_tupla))
+print('O resultado da soma dos números da tupla é igual a',somar(numeros_tupla))
