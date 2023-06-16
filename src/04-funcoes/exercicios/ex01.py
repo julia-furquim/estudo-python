c8f6c5279ebe23b2f012df964b98dd7487b19fef
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2023-06-16 09:47:27 -0300

    exercício 1

---
+++ src/04-funcoes/exercicios/ex01.py
@@ -0,0 +1,9 @@
+""" crie uma função que recebe três números como 
+parâmetro (n1, n2, n3) e imprime na saída padrão a soma dos 
+números """
+
+def somar(n1, n2, n3):
+    print(f'O resultado da soma dos números {n1}, {n2}, {n3} é igual a:\n', n1 + n2 + n3)
+
+somar(3.0, 8.5, 5.0)    
+
